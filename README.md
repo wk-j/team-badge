c6e6efd462273dad28e4f5b713cfd40ac604a222
wk-j <somnuk.wk@gmail.com>
2019-04-22 16:24:08 +0700

    Change URL

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Team Badge
 
+[![NuGet](https://img.shields.io/nuget/v/wk.TeamBadge.svg)](https://www.nuget.org/packages/wk.TeamBadge)
+
 ```bash
 dotnet tool install -g wk.TeamBadge
 wk-team-badge AsposeService
--- src/TeamBadge/Program.cs
+++ src/TeamBadge/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 
-namespace TeamBadge
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace TeamBadge {
+    class Program {
+        static void Main(string[] args) {
             var template = @"
 [![](http://bcircle.hopto.org:8111/app/rest/builds/buildType:{id}_Build/statusIcon)](http://bcircle.hopto.org:8111/viewType.html?buildTypeId={id}_Build)";
-            var r = template.Replace("{id}", args[0]);
+
+            var r = template
+                .Trim()
+                .Replace("{id}", args[0])
+                .Replace("http://bcircle.hopto.org:8111", "http://183.88.235.202:8111");
+
             Console.WriteLine(r);
         }
     }
